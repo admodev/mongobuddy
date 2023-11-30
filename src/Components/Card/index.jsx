011e38fda4333cc04885e4f0b59f6f40f3cd6cc8
admodev <adolfohectormoyano@gmail.com>
2023-11-30 12:36:01 -0300

    add database and collection selector

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -7,6 +7,28 @@ import './styles.css';
 const CardComponent = (props) => {
   return(
     <div className='card'>
+      <div className='cardHeader'>
+        <h4 className='cardHeaderTitle'>Database: {props.database ?? ''}</h4>
+        <div className="select">
+          <select>
+            {!props.database ? (<option>--Select--</option>) : props.database.forEach((db) => (
+              <option>{db.name}</option>
+            ))}
+          </select>
+          <div className="select_arrow">
+          </div>
+        </div>
+        <h4 className='cardHeaderTitle'>Collection: {props.collection ?? ''}</h4>
+        <div className="select">
+          <select>
+            {!props.collection ? (<option>--Select--</option>) : props.collection.forEach((col) => (
+              <option>{col.name}</option>
+            ))}
+          </select>
+          <div className="select_arrow">
+          </div>
+        </div>
+      </div>
       {props.children}
     </div>
   );
